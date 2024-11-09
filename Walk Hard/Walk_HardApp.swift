103940c44eb3453eda27fb86d3f274ebcbb95985
Nikolai <downsnikolai@gmail.com>
2024-11-09 02:15:45 -0800

    Initial Commit

---
+++ Walk Hard/Walk_HardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Walk_HardApp.swift
+//  Walk Hard
+//
+//  Created by Nikolai Downs on 11/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Walk_HardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
